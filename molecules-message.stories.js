11efba09cd8a3e055aecd648e3efe444eb270c0b
nftbit <t.kawamoto@4nonome.com>
2023-02-10 18:53:53 +0900

    Add files via upload

---
+++ molecules-message.stories.js
@@ -0,0 +1,6 @@
+import Item_Message from "../components/molecules/massage/message.html";
+import "../components/molecules/massage/message.scss";
+
+export default { title: "Molecules/Message"};
+
+export const Message = () => Item_Message; 
